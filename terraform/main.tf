cfe7717dd4b9d49d8dd7fac696d5a96c102fd65d
pruebaautomatizacionia <prueba.automatizacion.ia@gmail.com>
2025-12-18 14:00:16 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,20 @@ terraform {
 }
 
 data "aws_vpc" "default" { default = true }
-data "aws_subnets" "default" { filter { name="vpc-id"; values=[data.aws_vpc.default.id] } }
-data "aws_ami" "linux" { most_recent = true; owners = ["amazon"]; filter { name = "name"; values = ["amzn2-ami-hvm-*-x86_64-gp2"] } }
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+data "aws_ami" "linux" { 
+  most_recent = true
+  owners = ["amazon"]
+  filter { 
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"] 
+  } 
+}
 
 variable "key_name" { type = string }
 
