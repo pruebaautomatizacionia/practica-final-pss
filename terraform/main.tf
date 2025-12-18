af90cf9cfc53aea0cf852cbc49e0a982951d59e0
pruebaautomatizacionia <prueba.automatizacion.ia@gmail.com>
2025-12-18 16:06:16 +0100

    contenido terraform añadido

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,79 @@
-terraform {
+rm {
   backend "s3" {
     bucket = "infrabot-tf-state-pruebaautomatizacionia"
-    key    = "terraform/state.tfstate"
+    key = "terraform/state.tfstate"
     region = "eu-north-1"
     dynamodb_table = "terraform-lock-table-infrabot"
   }
-}
 
-data "aws_vpc" "default" { default = true }
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  variables {
+    key_name = string
   }
-}
-data "aws_ami" "linux" { 
-  most_recent = true
-  owners = ["amazon"]
-  filter { 
-    name = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"] 
-  } 
-}
 
-variable "key_name" { type = string }
+  data "aws_vpc" "default" {
+    default = true
+  }
 
-resource "aws_instance" "web" {
-  count = 3
-  ami           = data.aws_ami.linux.id
-  instance_type = "t3.micro"
-  subnet_id     = data.aws_subnets.default.ids[0]
-  key_name      = var.key_name
-}
+  data "aws_subnets" "default" {
+    vpc_id = data.aws_vpc.default.id
+    filter {
+      name   = "tag:Name"
+      values = ["subnet-privado"]
+    }
+  }
 
-output "vm_public_ips" { value = aws_instance.web[*].public_ip }
+  data "aws_ami" "linux" {
+    most_recent = true
+
+    filter {
+      name   = "name"
+      values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+
+    filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+    }
+
+    owners = ["amazon"]
+  }
+
+  resource "aws_security_group" "ansible_access" {
+    name        = "ansible_access"
+    description = "Security group for Ansible access"
+
+    ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 0
+      to_port   = 0
+      protocol  = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  resource "aws_instance" "web" {
+    count = 3
+
+    instance_type = "t3.micro"
+    subnet_id     = data.aws_subnets.default.ids[0]
+    vpc_security_group_ids = [aws_security_group.ansible_access.id]
+    ami             = data.aws_ami.linux.id
+    key_name        = var.key_name
+  }
+
+  output "vm_public_ips" {
+    value = aws_instance.web[*].public_ip
+ 
