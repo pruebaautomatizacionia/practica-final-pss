14d9bd816d7a39200524a4b5c068ca62a420693e
pruebaautomatizacionia <prueba.automatizacion.ia@gmail.com>
2025-12-18 11:36:39 +0100

    contenido terraform añadido

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,75 +4,41 @@ terraform {
     key            = "terraform/state.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-lock-table-infrabot"
-    encrypt        = true
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
+variable "key_name" {
+  type = string
 }
 
-locals {
-  common_tags = {
-    Name        = "Transfer International Project"
-    Environment = "dev"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name="vpc-id"
+    values=[data.aws_vpc.default.id]
   }
 }
 
-data "aws_ami" "centos8" {
+data "aws_ami" "linux" {
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["CentOS Linux 8*x64 HVM ENA Support*GP2*NA*amzn2-ami-hvm-2.0.xxxx-xx-amd64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
   owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
-variable "vpc_id" {}
-variable "subnet_ids" {}
-variable "key_name" {}
-
-### EC2 INSTANCES ###
-resource "aws_instance" "ec2" {
-  count         = 3
-  ami           = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
-  subnet_id     = element(var.subnet_ids, count.index)
+resource "aws_instance" "web" {
+  count = 3
+  ami           = data.aws_ami.linux.id
+  instance_type = "t2.micro"
   key_name      = var.key_name
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "ec2-instance-${count.index + 1}"
-    }
-  )
+  subnet_id     = data.aws_subnets.default.ids[0]
 }
 
-### RDS ###
-resource "aws_db_subnet_group" "rds" {
-  name       = "rds-subnet-group"
-  subnet_ids = var.subnet_ids
-
-  tags = local.common_tags
-}
-
-resource "aws_db_instance" "rds" {
-  identifier              = "rds-dev"
-  engine                  = "mysql"
-  engine_version          = "8.0"
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
-  username                = "admin"
-  password                = "password123"
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_db_subnet_group.rds.name
-
-  tags = local.common_tags
+output "vm_public_ips" {
+  value = aws_instance.web.*.public_ip
 }
