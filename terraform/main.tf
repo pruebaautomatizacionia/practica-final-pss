8d17786f583ecec9c1e55532513b47b105926564
Claudia Arrate Esteve <carrate@pss-ti.com>
2025-12-19 12:21:38 +0100

    fmt hecho

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,31 @@
 terraform {
   backend "s3" {
-    bucket = "infrabot-tf-state-pruebaautomatizacionia"
-    key    = "terraform/state.tfstate"
-    region = "eu-north-1"
+    bucket         = "infrabot-tf-state-pruebaautomatizacionia"
+    key            = "terraform/state.tfstate"
+    region         = "eu-north-1"
     dynamodb_table = "terraform-lock-table-infrabot"
   }
 }
- data "aws_vpc" "default" {
-   default = true
- }
+data "aws_vpc" "default" {
+  default = true
+}
 
- data "aws_subnets" "default" {
+data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
- }
+}
 
- variable "key_name" {
-   type = string
- }
+variable "key_name" {
+  type = string
+}
 
- provider "aws" {
-   region = "eu-north-1"
- }
+provider "aws" {
+  region = "eu-north-1"
+}
 
- data "aws_ami" "linux" {
+data "aws_ami" "linux" {
   most_recent = true
   owners      = ["amazon"]
 
@@ -40,46 +40,46 @@ terraform {
   }
 }
 
- resource "aws_security_group" "ansible_access" {
-   name        = "ansible_access"
-   description = "Allow SSH and MySQL access"
+resource "aws_security_group" "ansible_access" {
+  name        = "ansible_access"
+  description = "Allow SSH and MySQL access"
 
-   ingress {
-     from_port = 22
-     to_port   = 22
-     protocol  = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-   ingress {
-     from_port = 3306
-     to_port   = 3306
-     protocol  = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-   egress {
-     from_port = 0
-     to_port   = 0
-     protocol  = "-1"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
- }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
- resource "aws_instance" "web" {
-   count = 3
+resource "aws_instance" "web" {
+  count = 3
 
-   instance_type = "t3.micro"
+  instance_type = "t3.micro"
 
-   ami           = data.aws_ami.linux.id
+  ami = data.aws_ami.linux.id
 
-   subnet_id     = data.aws_subnets.default.ids[0]
+  subnet_id = data.aws_subnets.default.ids[0]
 
-   vpc_security_group_ids = [aws_security_group.ansible_access.id]
+  vpc_security_group_ids = [aws_security_group.ansible_access.id]
 
-   key_name = var.key_name
- }
+  key_name = var.key_name
+}
 
 output "vm_public_ips" {
-   value = aws_instance.web[*].public_ip
+  value = aws_instance.web[*].public_ip
 }
