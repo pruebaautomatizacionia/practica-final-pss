09a759f0a4d915adc723bc45b4d9f1d7896101ae
pruebaautomatizacionia <prueba.automatizacion.ia@gmail.com>
2025-12-19 09:24:25 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,23 +26,19 @@ terraform {
  }
 
  data "aws_ami" "linux" {
-   most_recent = true
+  most_recent = true
+  owners      = ["amazon"]
 
-   filter {
-     name   = "name"
-     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-   }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 
-   filter {
-     name   = "virtualization-type"
-     values = ["hvm"]
-   }
-
-   filter {
-     name   = "root-device-type"
-     values = ["ebs"]
-   }
- }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
  resource "aws_security_group" "ansible_access" {
    name        = "ansible_access"
