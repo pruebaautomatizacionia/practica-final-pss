40e2e3edc9c4d3084d1db202af52787f4ade10bc
pruebaautomatizacionia <prueba.automatizacion.ia@gmail.com>
2025-12-19 14:53:44 +0100

    contenido terraform añadido

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,83 @@
+e "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-resource "aws_instance" "transferencia-internacional" {
-  count = 2
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
-  ami           = data.aws_ami.generic.id
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 0)
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "web_ssh" {
+  vpc_id = aws_vpc.main.id
+  name   = "web_ssh_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "vm_1" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.public_1.id
+  security_groups = [aws_security_group.web_ssh.name]
+  key_name        = var.key_name
 
   tags = {
     Environment = var.environment
-    Project     = var.project
+    Project     = "transferencia internacional"
     Owner       = var.owner
   }
 }
+
+resource "aws_instance" "vm_2" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.public_2.id
+  security_groups = [aws_security_group.web_ssh.name]
+  key_name        = var.key_name
+
+  tags = {
+    Environment = var.environment
+    Project     = "transferencia internacional"
+    Owner       = var.owner
+  
