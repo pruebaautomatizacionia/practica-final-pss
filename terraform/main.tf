4c304da694ff77d238d3c4d1296724cbc0005b90
pruebaautomatizacionia <prueba.automatizacion.ia@gmail.com>
2025-12-19 14:01:57 +0100

    contenido terraform añadido

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,23 +12,3 @@ resource "aws_instance" "transferencia-internacional" {
     Owner       = var.owner
   }
 }
-
-resource "aws_db_instance" "rds_instance" {
-  allocated_storage      = 20
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class          = "db.t2.micro"
-  username               = "your-username"
-  password               = "your-password"
-  parameter_group_name   = "default.mysql8.0"
-  db_subnet_group_name    = var.vpc_id != "" ? aws_db_subnet_group.main.name : ""
-  vpc_security_group_ids  = [aws_security_group.allow_all.id]
-  publicly_accessible     = true
-
-  tags = {
-    Environment = var.environment
-    Project     = var.project
-    Owner       = var.owner
-  }
-}
